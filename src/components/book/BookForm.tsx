5cb82c4dbccd57f39896efcef3be13a14ae5df2e
Marcin Stefaniak <marcin.stefaniak@auto1.com>
2020-09-22 10:31:00 +0200

    add form error handler

--- src/components/book/BookForm.tsx
+++ src/components/book/BookForm.tsx
@@ -2,24 +2,30 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Form, Field } from 'react-final-form';
 
-import { Button, Grid, Typography, FormControl, InputLabel, Input, FormHelperText } from '@material-ui/core';
+import { Button, Grid, Typography, TextField } from '@material-ui/core';
 
 interface FormValues {
-  email: string,
   name: string,
-}
+  email: string,
+};
+
+type ValidatorResponse = undefined | string;
+type Validator = (value?: string) => ValidatorResponse;
+
+const composeValidators = (...validators: Array<Validator>) => (value?: string): ValidatorResponse =>
+  validators.reduce((error: ValidatorResponse, validator) => error || validator(value), undefined);
 
 const BookForm = () => {
   const { t } = useTranslation();
+  const isNotEmpty: Validator = (value) => (value ? undefined : t('required'))
+  const isEmail = (email?: string): ValidatorResponse => (email && email?.indexOf('@') > -1 ? undefined : t('invalid_email'))
 
-  const onSubmit = (data: FormValues): void => {
-    console.log('submit', data);
+  const onSubmit = (values: FormValues): void => {
+    console.log('submit', values);
   }
 
-  const validate = () => {};
-
   return (
-    <Form<FormValues> onSubmit={onSubmit} validate={validate} render={({ handleSubmit }) => (
+    <Form onSubmit={onSubmit} render={({ handleSubmit }) => (
       <form onSubmit={handleSubmit}>
         <Grid container spacing={2}>
           <Grid item xs={12}>
@@ -27,18 +33,28 @@ const BookForm = () => {
             <Typography variant="h6" gutterBottom>The Office - Craft Beer Pub</Typography>
           </Grid>
           <Grid item xs={12}>
-            <FormControl>
-              <InputLabel htmlFor="email">{t('email')}</InputLabel>
-              <Input id="email" aria-describedby="email-helper" />
-              <FormHelperText id="email-helper">{t('email_hint')}</FormHelperText>
-            </FormControl>
+            <Field name="email" validate={composeValidators(isNotEmpty, isEmail)}>
+            {({ input, meta }) => (
+              <TextField
+                {...input}
+                label={meta.touched && meta.error ? t('error') : t('email')}
+                error={meta.touched && !!meta.error}
+                helperText={meta.touched && meta.error ? meta.error : t('email_hint')}
+              />
+            )}
+            </Field>
           </Grid>
           <Grid item xs={12}>
-            <FormControl>
-              <InputLabel htmlFor="name">{t('name')}</InputLabel>
-              <Input id="name" aria-describedby="name-helper" />
-              <FormHelperText id="name-helper">{t('name_hint')}</FormHelperText>
-            </FormControl>
+            <Field name="name" validate={isNotEmpty}>
+              {({ input, meta }) => (
+                <TextField
+                  {...input}
+                  label={meta.touched && meta.error ? t('error') : t('name')}
+                  error={meta.touched && !!meta.error}
+                  helperText={meta.touched && meta.error ? meta.error : t('name_hint')}
+                />
+              )}
+            </Field>
           </Grid>
           <Grid item xs={12}>
             <Button type="submit" variant="contained" color="primary">
--- src/locale/en.ts
+++ src/locale/en.ts
@@ -3,6 +3,9 @@ export default {
   book_a_table: 'Book a table',
   email: 'Email address',
   email_hint: 'We\'ll never share your email.',
+  email_invalid: 'Invalid email',
+  error: 'Error',
   name: 'Name',
   name_hint: 'Name or nickname',
+  required: 'Required',
 }
