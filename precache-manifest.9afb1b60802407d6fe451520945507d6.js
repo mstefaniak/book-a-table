d2f900a4b602da8004e8014daa49d90454ed520b
Marcin Stefaniak <marcin.stefaniak@auto1.com>
2020-10-07 10:58:28 +0200

    Updates

---
+++ precache-manifest.9afb1b60802407d6fe451520945507d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45e947a5968b7af8eaa389b4375b7b4b",
+    "url": "http://mstefaniak.github.io/book-a-table/index.html"
+  },
+  {
+    "revision": "246eb0eda96efdb23bd2",
+    "url": "http://mstefaniak.github.io/book-a-table/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "40d8e259cef44ccad7d2",
+    "url": "http://mstefaniak.github.io/book-a-table/static/js/2.a99fab8e.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "http://mstefaniak.github.io/book-a-table/static/js/2.a99fab8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "246eb0eda96efdb23bd2",
+    "url": "http://mstefaniak.github.io/book-a-table/static/js/main.5361bd76.chunk.js"
+  },
+  {
+    "revision": "414b62d2c1ec50af876e",
+    "url": "http://mstefaniak.github.io/book-a-table/static/js/runtime-main.c2090393.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dec6878ddda27052d6029b71ccf5da53.js"
+  "http://mstefaniak.github.io/book-a-table/precache-manifest.9afb1b60802407d6fe451520945507d6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("http://mstefaniak.github.io/book-a-table/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
