404ec58cf1fc47d12195604e9e3c1136dd75bfbb
Marcin Stefaniak <marcin.stefaniak@auto1.com>
2020-10-07 09:00:03 +0200

    Updates

---
+++ precache-manifest.53a70745230aae1c81759987ab854f29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40eb177fffb0309e09aba40f740c1816",
+    "url": "/index.html"
+  },
+  {
+    "revision": "465ddb15758d4177a9e2",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0ba64750a1994eb2a8d0",
+    "url": "/static/js/2.a06f9648.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/static/js/2.a06f9648.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "465ddb15758d4177a9e2",
+    "url": "/static/js/main.2edf8ae3.chunk.js"
+  },
+  {
+    "revision": "f9bd60f2ac749c377383",
+    "url": "/static/js/runtime-main.8e901099.js"
+  }
+]);
